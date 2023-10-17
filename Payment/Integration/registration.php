0fa89c2079e632a4bd15ad230f26514dcbde83da
Jean Damaceno <jeandamaceno3@gmail.com>
2023-10-17 17:56:18 -0300

    Criacao de diretorios e declaracao dos arquivos de registro

---
+++ Payment/Integration/registration.php
@@ -0,0 +1,7 @@
+<?php
+  declare(strict_types=1);
+  /* @author Jean Damaceno jeandamaceno3@gmail.com */
+
+    use Magento\Framework\Component\ComponentRegistrar;
+
+    ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Payment_Integration', __DIR__);
